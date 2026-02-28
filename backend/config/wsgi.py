3f70db10946d848897275b6a2b42cdb9ec6466be
maks march <1maks_2055@mail.ru>
2026-02-28 16:24:58 +0500

    РАБОТАЕТ

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+jarvis_path = os.path.join(BASE_DIR, 'jarvis')
+
+if jarvis_path not in sys.path:
+    sys.path.insert(0, jarvis_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
